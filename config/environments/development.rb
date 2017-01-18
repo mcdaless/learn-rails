db9bd03bd99628246dcf8eab94da656f824e5a1c
Carlo D'Alessio <dalessioc@gmail.com>
2017-01-17 22:12:14 -0500

    add configuration updates, syntax errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,13 +52,11 @@ Rails.application.configure do
     password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
-# Send email in development mode?
-config.action_mailer.perform_deliveries = true
-
-  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
